9b9d31d6bb7fb2f931e7f33ae6db1c23e0f00ae6
Aaron Lu <aaronnoh0409@gmail.com>
2020-10-25 18:29:53 -0400

    added varlues and API keys, also made some test elements in html so i can test out the js output

--- weather.js
+++ weather.js
@@ -1,1 +1,29 @@
-//helloooooooooooooooooooooooooooooooooooo
+//adding API key
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+// all the vars that I need
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+
+
+// AJAX call
+function currentWeather(city){
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET", 
+    }).then(function(response){
+        console.log(response);
+        //Setting up weather icons
+        var weathericon= response.weather[0].icon;
+        var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
+        
+    
+    })
+}
