1b34e0a6c90e09d91ff835fd6afd8784954afedc
Aaron Lu <aaronnoh0409@gmail.com>
2020-10-25 18:46:15 -0400

    still working on linking api to the page

--- weather.js
+++ weather.js
@@ -23,7 +23,10 @@ function currentWeather(city){
         //Setting up weather icons
         var weathericon= response.weather[0].icon;
         var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
-        
+        //get date
+        var date=new Date(response.dt*1000).toLocaleDateString();
+        //Icon will change depends on the ananlysis
+        $(currentCity).html(response.name +"("+date+")" + "<img src="+iconurl+">");
     
     })
 }
