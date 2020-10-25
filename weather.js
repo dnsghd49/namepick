03a13d24422874bb46b80fb2ae51f6b9978ba6e2
Aaron Lu <aaronnoh0409@gmail.com>
2020-10-25 19:14:31 -0400

    added uvindex but it does not do anything honesyly and i dont know why

--- weather.js
+++ weather.js
@@ -34,13 +34,32 @@ function currentWeather(city){
         
         //outputs humidity
         $(currentHumidty).html(response.main.humidity+"%");
-        
+
         //windspeed
         var ws=response.wind.speed;
         var windsmph=(ws*2.237).toFixed(1);
         $(currentWSpeed).html(windsmph+"MPH");
-
-
+        //output uv index 
+        UVIndex(response.coord.lon,response.coord.lat);
+        forecast(response.id);
+        if(response.cod==200){
+            sCity=JSON.parse(localStorage.getItem("cityname"));
+            console.log(sCity);
+            if (sCity==null){
+                sCity=[];
+                sCity.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityname",JSON.stringify(sCity));
+                addToList(city);
+            }
+            else {
+                if(find(city)>0){
+                    sCity.push(city.toUpperCase());
+                    localStorage.setItem("cityname",JSON.stringify(sCity));
+                    addToList(city);
+                }
+            }
+        }
     })
 }
 
